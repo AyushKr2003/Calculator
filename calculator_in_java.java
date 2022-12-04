8004a47a4db1613f5b3fe00e5e238aafcaf2a56c
Ayush Kumar Singh <112181404+AyushKr2003@users.noreply.github.com>
2022-12-04 21:29:04 +0530

    Create calculator_in_java.java

---
+++ calculator_in_java.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        float num1,num2;
+        System.out.print("Enter the first number:");
+        num1 = scan.nextFloat();
+        System.out.print("Enter the second number:");
+        num2 = scan.nextFloat();
+
+        String p = "Enter 1 for Addition\n2 for Subtraction \n3 for Multiplication \n4 for Division";
+        System.out.println(p);
+        System.out.print("Enter your choice:");
+
+        int op = scan.nextInt();
+
+        switch (op){
+            case 1:
+                System.out.println(num1 +" + " +num2 + " = " + (num1+num2));
+                break;
+
+            case 2:
+                System.out.println(num1 +" - " +num2 + " = " + (num1-num2));
+                break;
+
+            case 3:
+                System.out.println(num1 +" * " +num2 + " = " + (num1*num2));
+                break;
+
+            case 4:
+                System.out.println(num1 +" / " +num2 + " = " + (num1/num2));
+                break;
+
+            default:
+                System.out.println("Invalid Input.");
+                break;
+        }
+
+    }
+}
